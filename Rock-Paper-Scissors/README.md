f7fb732d4b7b27b12612a1c552cb8caf3ea3bafc
prasanthpatnaik <prasanthpatnaikvandrangi@gmail.com>
2023-12-15 19:11:38 +0530

    Building Rock Paper Scissors Game

---
+++ Rock-Paper-Scissors/README.md
@@ -0,0 +1,26 @@
+# Rock Paper Scissors
+
+In this project, weare going to build the `Rock Paper Scissors` game using HTML, CSS and JS.
+
+### Rules -
+
+This game has 2 players and each player can pick one of the following 3 choices-
+
+- Rock
+- Paper
+- Scissors
+
+Both the players compare their pick with each other and whoever has the greater pick wins.
+
+The order of the choices is as follows-
+`Rock > Scissors`
+`Scissors > Paper`
+`Paper > Rock`
+
+##### Example-
+
+When Player1 chooses Rock and Player2 chooses Scissors, `Rock beats Scissors` and Player1 wins.
+When Player1 chooses Paper and Player2 chooses Scissors, `Scissors cuts Paper` and Player2 wins.
+When Player1 chooses Rock and Player2 chooses Paper, `Paper wraps Rock` and Player2 wins.
+
+_From Apna College's [MiniProject - Stone, Paper & Scissors Game | Lecture 10 of JavaScript Full Course](https://youtu.be/_V33HCZWLDQ?list=PLGjplNEQ1it_oTvuLRNqXfz_v_0pq6unW)_
---
+++ Rock-Paper-Scissors/script.js
@@ -0,0 +1,65 @@
+let userScore = 0;
+let compScore = 0;
+
+let choices = document.querySelectorAll(".choice");
+let msg = document.querySelector("#msg");
+let userScoreDisplay = document.querySelector("#user-score");
+let compScoreDisplay = document.querySelector("#comp-score");
+
+const genCompChoice = () => {
+    let options = ["rock", "paper", "paper"];
+    let randIdx = Math.floor(Math.random() * 3);
+    return options[randIdx];
+};
+
+const showWinner = (userWin) => {
+    if (userWin) {
+        userScore++;
+        userScoreDisplay.innerText = userScore;
+        msg.innerText = "You win!";
+        msg.style.backgroundColor = "darkgreen";
+    } else {
+        compScore++;
+        compScoreDisplay.innerText = compScore;
+        msg.innerText = "You lose.";
+        msg.style.backgroundColor = "darkred";
+    }
+};
+
+const showDraw = () => {
+    msg.innerText = "It's a draw.";
+    msg.style.backgroundColor = "#081b31";
+};
+
+const playGame = (userChoice) => {
+    let compChoice = genCompChoice();
+    let userWin = true;
+    let isDraw = false;
+
+    if (userChoice === compChoice) {
+        isDraw = true;
+    } else if (userChoice === "rock") {
+        // compChoice will be paper or scissors
+        userWin = compChoice === "paper" ? false : true;
+    } else if (userChoice === "paper") {
+        //compChoice will be rock or scissors
+        userWin = compChoice === "scissors" ? false : true;
+    } else {
+        //userChoice is scissors
+        //compChoice will be rock or paper
+        userWin = compChoice === "rock" ? false : true;
+    }
+
+    if (!isDraw) {
+        showWinner(userWin);
+    } else {
+        showDraw();
+    }
+};
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        let userChoiceId = choice.getAttribute("id");
+        playGame(userChoiceId);
+    });
+});
